45e6083472707238b65c0ae67a6f32eb8ca7f28c
MacUser <macuser@macuseracbookea.home>
2026-04-11 02:19:12 +0200

    Add AI tab: chat, voice input, photo analysis with Cloudflare Worker backend

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ito-family-dash-v6';
+const CACHE_NAME = 'ito-family-dash-v7';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
