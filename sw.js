e9065e69a6865eb2a414cf139c64a22f30c3e0c2
MacUser <macuser@macuseracbookea.home>
2026-04-11 17:00:52 +0200

    Full Stranger Things: Vecna icon, clock lock screen, themed menus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ito-family-dash-v23';
+const CACHE_NAME = 'ito-family-dash-v24';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './bg.webp'];
 
 self.addEventListener('install', e => {
