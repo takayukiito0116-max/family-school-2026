127b94d3734580bbfb049834f6786b90369182ed
MacUser <macuser@macuseracbookea.home>
2026-04-11 15:29:22 +0200

    Chat fix: messages top + input bottom, no Enter send, reliable auto-scroll

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ito-family-dash-v18';
+const CACHE_NAME = 'ito-family-dash-v19';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './bg.webp'];
 
 self.addEventListener('install', e => {
